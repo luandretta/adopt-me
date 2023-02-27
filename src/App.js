f18711303eb447d4545ade7a100927f80f0514b8
Lucimeri Andretta <luandretta@hotmail.com>
2023-02-27 21:57:39 +0100

    Feat: Add prettier and node modules

--- src/App.js
+++ src/App.js
@@ -1,32 +1,28 @@
 const Pet = (props) => {
-    return React.createElement("div", {}, [
-      React.createElement("h1", {}, props.name),
-      React.createElement("h2", {}, props.animal),
-      React.createElement("h2", {}, props.breed),
-    ]);
-  };
-  
-  const App = () => {
-    return React.createElement("div", {}, [
-      React.createElement("h1", {}, "Adopt Me!"),
-      React.createElement(Pet, {
-        animal: "Dog",
-        name: "Luna",
-        breed: "Havanese"
-      }),
-      React.createElement(Pet,  {
-        animal: "Bird",
-        name: "Pepper",
-        breed: "Cockatiel"
-      }),
-      React.createElement(Pet, {
-         animal: "Cat",
-         name: "Doink",
-         breed: "Mixed"
-      }),
-    ]);
-  };
-  
-  const container = document.getElementById("root");
-  const root = ReactDOM.createRoot(container);
-  root.render(React.createElement(App));
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "Bird",
+      breed: "Cockatiel",
+    }),
+    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mix" }),
+  ]);
+};
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
